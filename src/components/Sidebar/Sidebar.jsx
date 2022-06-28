4db5e8f16a18294a51f1d45d9566e27aa6ba4b37
Elise <elise@webtag.fr>
2022-06-28 13:21:46 +0200

    sidebar resizable

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useCallback, useState, useEffect, useRef } from "react";
 import Datavis from "../Datavis/Datavis";
 import "./Sidebar.scss";
 
 export default function Sidebar({ checked, setChecked, setMoistVis}) {
+  const sidebarRef = useRef(null)
+  const [isResizing, setIsResizing] = useState(false)
+  const [sidebarWidth, setSidebarWidth] = useState(268)
+  
+  const startResizing = useCallback(() => {
+    setIsResizing(true)
+}, [])
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false)
+}, [])
+
+const resize = useCallback((mouseMoveEvent) => {
+    if (isResizing) {
+        setSidebarWidth(sidebarRef.current.getBoundingClientRect().right - mouseMoveEvent.clientX)
+    }
+}, [isResizing])
+
+useEffect(() => {
+    window.addEventListener("mousemove", resize)
+    window.addEventListener("mouseup", stopResizing)
+    return () => {
+        window.removeEventListener("mousemove", resize)
+        window.removeEventListener("mouseup", stopResizing)
+    }
+}, [resize, stopResizing])
+
   return (
-      <div id='sidebar'>
+      <div id='sidebar'
+      ref={sidebarRef}
+      style={{ width: sidebarWidth}}
+      onMouseDown={e => e.preventDefault()}>
+        <div className="grabbable"
+        onMouseDown={startResizing}>
+        </div>
         <Datavis
         checked={checked}
         setChecked={setChecked}
